cfa850b5410cf0a8318165efceb30c8ac9a563ed
Vinayak pandey <Vinayakpandey286@gmail.com>
2024-08-05 16:06:17 +0530

    added transfer page

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -7,14 +7,56 @@ const PORT = 3003;
 
 app.use(express.json());
 
-app.post("/hdfcWebhook", (req, res) => {
-  const paymentInformation = {
+app.post("/hdfcWebhook", async (req, res) => {
+  const paymentInformation: {
+    token: string;
+    userId: string;
+    amount: string;
+  } = {
     token: req.body.token,
-    userId: req.body.user_indentifier,
+    userId: req.body.user_identifier,
     amount: req.body.amount,
   };
+  try {
+    await db.$transaction([
+      db.balance.update({
+        where: {
+          userId: paymentInformation.userId,
+        },
+        data: {
+          amount: {
+            increment: Number(paymentInformation.amount),
+          },
+        },
+      }),
+      db.onRampTransaction.update({
+        where: {
+          token: paymentInformation.userId,
+        },
+        data: {
+          Status: "Success",
+        },
+      }),
+    ]);
 
-  db.balance.create;
+    res.json({
+      message:"Captured"
+    })
+  } catch (error) {
+    console.log(error)
+    await db.onRampTransaction.update({
+      where: {
+        token: paymentInformation.userId,
+      },
+      data: {
+        Status: "Failed",
+      },
+    });
+
+    res.status(411).json({
+      message:"Error while Processing webhook"
+    })
+  }
 });
 
 app.listen(PORT);
